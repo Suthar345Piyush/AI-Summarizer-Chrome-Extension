fb77c5a5a4a47181c8221ed883a1ffb477fd4662
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-04-22 17:39:26 +0530

    making chrome extension

---
+++ popup.js
@@ -0,0 +1,16 @@
+document.getElementById("summarize").addEventListener("click" , () => {
+   const result = document.getElementById("result");
+   result.textContent = "Extracting text....";
+
+
+   chrome.tabs.query({active : true , currentWindow : true} , ([tab]) => {
+      chrome.tabs.sendMessage(
+         tab.id,
+         {type : "GET_ARTICLE_TEXT"},
+         ({text}) => {
+           result.textContent = text ? text.slice(0 , 300) + "_" : "No article text found";
+         }
+      );
+   });
+});
+
